9b37d97262f15d129e6cf11598e85b0dd1b42120
Ederson Arruda Ceccato <edersonceccato@gmail.com>
2025-07-18 17:48:49 -0300

    ✨ feat - Ajustado icone de mostrar senha

--- app/public/assets/js/show-password.js
+++ app/public/assets/js/show-password.js
@@ -1,17 +1,16 @@
 
 "use strict"
 
-// for show password 
+// for show password
 let createpassword = (type, ele) => {
     document.getElementById(type).type = document.getElementById(type).type == "password" ? "text" : "password"
-    let icon = ele.childNodes[0].classList
-    let stringIcon = icon.toString()
-    if (stringIcon.includes("ri-eye-line")) {
-        ele.childNodes[0].classList.remove("ri-eye-line")
-        ele.childNodes[0].classList.add("ri-eye-off-line")
+    let icon = ele.querySelector('i').classList
+    if (icon.contains("ri-eye-off-line")) {
+        icon.remove("ri-eye-off-line")
+        icon.add("ri-eye-line")
     }
     else {
-        ele.childNodes[0].classList.add("ri-eye-line")
-        ele.childNodes[0].classList.remove("ri-eye-off-line")
+        icon.remove("ri-eye-line")
+        icon.add("ri-eye-off-line")
     }
 }
